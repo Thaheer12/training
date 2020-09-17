89de7c51c30f31db84615f600745d4e5db4b23a6
Thaheer <thaheer.t9@gmail.com>
2020-09-17 14:58:58 +0000

    rebase e Please enter the commit message for your changes. Lines starting

--- demo.java
+++ demo.java
@@ -5,3 +5,4 @@ Java Test File - Line 4
 Java Test File - Line 5
 Java Test File - Line 6 added in feature branch
 Java Test File - Line 7 added in feature branch for conflict
+Welcome to feature for rebase
